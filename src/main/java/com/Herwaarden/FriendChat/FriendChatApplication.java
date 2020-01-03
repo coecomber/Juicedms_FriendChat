2a2670394c2116f35061f0c38c64c4550f83dbcc
coecomber <joostvherwaarden@hotmail.com>
2020-01-03 15:05:03 +0100

    Initial commit

---
+++ src/main/java/com/Herwaarden/FriendChat/FriendChatApplication.java
@@ -0,0 +1,13 @@
+package com.Herwaarden.FriendChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendChatApplication.class, args);
+	}
+
+}
